8aebee17ae1d2d2fdf5319ed766662a41f61a8df
TRAI <nacari321@gmail.com>
2020-04-09 13:30:54 +0700

    fix form content

---
+++ Model/Dao/ContentDao.cs
@@ -0,0 +1,22 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    class ContentDao
+    {
+
+        WebShopDbContext db = null;
+        public long Insert(Content entity)
+        {
+            db.Contents.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+    }
+}
--- WebShop/Areas/Admin/Controllers/ContentController.cs
+++ WebShop/Areas/Admin/Controllers/ContentController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using Model.EF;
 
 namespace WebShop.Areas.Admin.Controllers
 {
@@ -16,6 +18,26 @@ namespace WebShop.Areas.Admin.Controllers
         public ActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create (Content content, HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+                return View("Create");
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+                return View("Create");
+            }
         }
     }
 }
