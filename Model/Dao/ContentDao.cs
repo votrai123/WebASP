2b036e30f82cc2d13327189e401c3f84c96083fb
TRAI <nacari321@gmail.com>
2020-04-10 13:04:41 +0700

    insert and delete content

--- Model/Dao/ContentDao.cs
+++ Model/Dao/ContentDao.cs
@@ -1,22 +1,44 @@
-﻿using Model.EF;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Model.EF;
+using PagedList;
 namespace Model.Dao
 {
-    class ContentDao
+    public class ContentDao
     {
 
         WebShopDbContext db = null;
+
+        public ContentDao()
+        {
+            db = new WebShopDbContext();
+        }
+        public IEnumerable<Content> ListAllPaping(int page, int pageSize)
+        {
+            return db.Contents.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
         public long Insert(Content entity)
         {
             db.Contents.Add(entity);
             db.SaveChanges();
             return entity.ID;
         }
-
+        public bool Delete(int id)
+        {
+            try
+            {
+                var content = db.Contents.Find(id);
+                db.Contents.Remove(content);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
--- WebShop/Areas/Admin/Controllers/ContentController.cs
+++ WebShop/Areas/Admin/Controllers/ContentController.cs
@@ -5,39 +5,70 @@ using System.Web;
 using System.Web.Mvc;
 using System.IO;
 using Model.EF;
+using Model.Dao;
+using WebShop.Common;
 
 namespace WebShop.Areas.Admin.Controllers
 {
     public class ContentController : Controller
     {
         // GET: Admin/Content
-        public ActionResult Index()
+        public ActionResult Index(int page = 1, int pageSize = 8)
         {
-            return View();
+            var dao = new ContentDao();
+            var model = dao.ListAllPaping(page, pageSize);
+            return View(model);
         }
         public ActionResult Create()
         {
             return View();
         }
-        [HttpPost]
+        [HttpPost, ValidateInput(false)]
         public ActionResult Create (Content content, HttpPostedFileBase file)
         {
             try
             {
-                if (file.ContentLength > 0)
+
+                if (file.ContentLength > 0 && ModelState.IsValid)
                 {
+                    var dao = new ContentDao();
+                    content.Status = true;
+                    content.ViewCount = 0;
+                    content.TopHot = DateTime.Now;
+                    content.CreatedDate = DateTime.Now;
                     string _FileName = Path.GetFileName(file.FileName);
-                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
-                    file.SaveAs(_path);
+                    string _path = Path.Combine(Server.MapPath("~/Areas/Admin/UploadedFiles"), _FileName);
+                    content.Image = _path; 
+                    long id = dao.Insert(content);
+                    if (id > 0)
+                    {
+                        file.SaveAs(_path);
+                        ViewBag.Message = "File Uploaded Successfully!!";
+                        ModelState.AddModelError("", "Them  thanh cong");
+                        return RedirectToAction("Index", "Content");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Them khong thanh cong");
+                    }
+
                 }
-                ViewBag.Message = "File Uploaded Successfully!!";
-                return View("Create");
+                return View("Index", "Content");
+
+
+
             }
             catch
             {
                 ViewBag.Message = "File upload failed!!";
                 return View("Create");
             }
+        }
+        public ActionResult Delete(int id)
+        {
+            new ContentDao().Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
