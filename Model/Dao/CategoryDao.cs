194f41b8142cdba3265110a65bf0839d8f738280
TRAI <nacari321@gmail.com>
2020-04-12 18:55:32 +0700

    done category

---
+++ Model/Dao/CategoryDao.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+using PagedList;
+
+namespace Model.Dao
+{
+    public class CategoryDao
+    {
+        WebShopDbContext db = null;
+        public CategoryDao()
+        {
+            db = new WebShopDbContext();
+        }
+        public long Insert(Category entity)
+        {
+            db.Categories.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public IEnumerable<Category> ListAllPaping(string searchString, int page, int pageSize)
+        {
+            IQueryable<Category> model = db.Categories;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Name.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+        public Category ViewDetail(int id)
+        {
+            return db.Categories.Find(id);
+        }
+        public bool Update(Category entity)
+        {
+            try
+            {
+                var category = db.Categories.Find(entity.ID);
+
+                category.Name = entity.Name;
+                category.Status = entity.Status;
+                category.ModifiedDate = DateTime.Now;
+                category.MetaDescriptions = entity.MetaDescriptions;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ WebShop/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.EF;
+using Model.Dao;
+using WebShop.Common;
+
+namespace WebShop.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Admin/Category
+        public ActionResult Index(string searchString, int page =1, int pageSize =5)
+        {
+            var dao = new CategoryDao();
+            var model = dao.ListAllPaping(searchString, page, pageSize);
+            ViewBag.searchString = searchString;
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var dao = new CategoryDao();
+                category.ShowOnHome = true;
+
+                category.Status = true;
+                category.CreatedDate = DateTime.Now;
+                long id = dao.Insert(category);
+                if (id > 0)
+                {
+                    return RedirectToAction("Index", "Category");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Them category khong thanh cong");
+                }
+            }
+            return View("Create");
+        }
+
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            var category = new CategoryDao().ViewDetail(id);
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult Update(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var dao = new CategoryDao();
+
+
+                category.ModifiedDate = DateTime.Now;
+                var result = dao.Update(category);
+                if (result)
+                {
+                    return RedirectToAction("Index", "Category");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cap nhat category khong thanh cong");
+                }
+            }
+            return View("Update");
+        }
+    }
+}
